59bc8af56f00644fa8385a0a70cfe9fb8c9dcc07
Leonardo Llanes <llanesleonardo@gmail.com>
2021-07-02 18:48:04 -0500

    Frontend dia 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,157 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Post from "./components/Post";
+const posts =[
+   { "id": 1,
+    "Nombre": "Post 1",
+    "Slug": "post-1",
+    "body": "Este es el post 1, una pequeña descripción.\n\n![ANTIGENO ROCHE .png](/uploads/ANTIGENO_ROCHE_403542253a.png)\n\nCon una imagen de roche.\n",
+    "fecha_creacion": "2021-07-02T17:00:00.000Z",
+    "Status": true,
+    "author": {
+        "id": 1,
+        "username": "Usuario 1",
+        "email": "user@gmail.com",
+        "provider": "local",
+        "confirmed": true,
+        "blocked": false,
+        "role": 1,
+        "created_at": "2021-07-02T19:58:59.670Z",
+        "updated_at": "2021-07-02T19:58:59.682Z"
+    },
+    "published_at": "2021-07-02T19:59:06.269Z",
+    "created_at": "2021-07-02T19:55:18.858Z",
+    "updated_at": "2021-07-02T20:00:52.212Z",
+    "img_ppal": {
+        "id": 1,
+        "name": "Cubrebocas KN95 .jpg",
+        "alternativeText": "",
+        "caption": "",
+        "width": 718,
+        "height": 661,
+        "formats": {
+            "thumbnail": {
+                "name": "thumbnail_Cubrebocas KN95 .jpg",
+                "hash": "thumbnail_Cubrebocas_KN_95_6250288cb4",
+                "ext": ".jpg",
+                "mime": "image/jpeg",
+                "width": 169,
+                "height": 156,
+                "size": 6.56,
+                "path": null,
+                "url": "/uploads/thumbnail_Cubrebocas_KN_95_6250288cb4.jpg"
+            },
+            "small": {
+                "name": "small_Cubrebocas KN95 .jpg",
+                "hash": "small_Cubrebocas_KN_95_6250288cb4",
+                "ext": ".jpg",
+                "mime": "image/jpeg",
+                "width": 500,
+                "height": 460,
+                "size": 33.58,
+                "path": null,
+                "url": "/uploads/small_Cubrebocas_KN_95_6250288cb4.jpg"
+            }
+        },
+        "hash": "Cubrebocas_KN_95_6250288cb4",
+        "ext": ".jpg",
+        "mime": "image/jpeg",
+        "size": 56.53,
+        "url": "/uploads/Cubrebocas_KN_95_6250288cb4.jpg",
+        "previewUrl": null,
+        "provider": "local",
+        "provider_metadata": null,
+        "created_at": "2021-07-02T19:52:15.212Z",
+        "updated_at": "2021-07-02T19:52:15.226Z"
+    }
+  },
+  { "id": 2,
+  "Nombre": "Post 1",
+  "Slug": "post-1",
+  "body": "Este es el post 1, una pequeña descripción.\n\n![ANTIGENO ROCHE .png](/uploads/ANTIGENO_ROCHE_403542253a.png)\n\nCon una imagen de roche.\n",
+  "fecha_creacion": "2021-07-02T17:00:00.000Z",
+  "Status": true,
+  "author": {
+      "id": 1,
+      "username": "Usuario 1",
+      "email": "user@gmail.com",
+      "provider": "local",
+      "confirmed": true,
+      "blocked": false,
+      "role": 1,
+      "created_at": "2021-07-02T19:58:59.670Z",
+      "updated_at": "2021-07-02T19:58:59.682Z"
+  },
+  "published_at": "2021-07-02T19:59:06.269Z",
+  "created_at": "2021-07-02T19:55:18.858Z",
+  "updated_at": "2021-07-02T20:00:52.212Z",
+  "img_ppal": {
+      "id": 1,
+      "name": "Cubrebocas KN95 .jpg",
+      "alternativeText": "",
+      "caption": "",
+      "width": 718,
+      "height": 661,
+      "formats": {
+          "thumbnail": {
+              "name": "thumbnail_Cubrebocas KN95 .jpg",
+              "hash": "thumbnail_Cubrebocas_KN_95_6250288cb4",
+              "ext": ".jpg",
+              "mime": "image/jpeg",
+              "width": 169,
+              "height": 156,
+              "size": 6.56,
+              "path": null,
+              "url": "/uploads/thumbnail_Cubrebocas_KN_95_6250288cb4.jpg"
+          },
+          "small": {
+              "name": "small_Cubrebocas KN95 .jpg",
+              "hash": "small_Cubrebocas_KN_95_6250288cb4",
+              "ext": ".jpg",
+              "mime": "image/jpeg",
+              "width": 500,
+              "height": 460,
+              "size": 33.58,
+              "path": null,
+              "url": "/uploads/small_Cubrebocas_KN_95_6250288cb4.jpg"
+          }
+      },
+      "hash": "Cubrebocas_KN_95_6250288cb4",
+      "ext": ".jpg",
+      "mime": "image/jpeg",
+      "size": 56.53,
+      "url": "/uploads/Cubrebocas_KN_95_6250288cb4.jpg",
+      "previewUrl": null,
+      "provider": "local",
+      "provider_metadata": null,
+      "created_at": "2021-07-02T19:52:15.212Z",
+      "updated_at": "2021-07-02T19:52:15.226Z"
+  }
+}
+
+  ];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <Container className="post_centrado">
+    <Row>
+            {posts.map(post =>(
+              <Post 
+              key={post.id}
+              title={post.Nombre} 
+              url={post.img_ppal && post.img_ppal.url} 
+              description={post.body} 
+              creation_date={post.created_at} 
+              />
+             ))}
+    </Row>
+  </Container>
     </div>
   );
 }
---
+++ src/components/Post.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
+
+export default (props) =>{
+ 
+    const {title,url,description,creation_date} = props;
+ 
+    console.log("url",url);
+    console.log("description",description);
+    console.log("creacion_date",creation_date);
+
+    const API_URL = 'http://localhost:1337';
+    const formatImage = (url) =>`${API_URL}${url}`;
+    
+    return(
+    <>
+        <Card className="Post" style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={formatImage(url)} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <small>{creation_date}</small>
+                <Card.Text>
+                {description}
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    </>
+    )    
+}
